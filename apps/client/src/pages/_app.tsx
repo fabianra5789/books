0c4d5e05c3a611d08f9c661b4937639121ff8108
Eric Ramirez <fabianra5789@gmail.com>
2023-09-02 17:43:25 -0500

    CP: add new UI library

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+// Chakra UI
+import chakraTheme from "@chakra-ui/theme";
+import { ChakraBaseProvider, extendBaseTheme } from "@chakra-ui/react";
+// Extends
+const { Button } = chakraTheme.components;
+// Theme
+//  Add your color mode config
+export const config = {
+  initialColorMode: "light",
+  useSystemColorMode: false,
+};
+const theme = extendBaseTheme({
+  components: {
+    Button,
+  },
+  config,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-       
+  return (
+    <ChakraBaseProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraBaseProvider>
+  );
 }
