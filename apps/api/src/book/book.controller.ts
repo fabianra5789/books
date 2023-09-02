ee7254738aa704c0ee77ada128931570fcbb1cfd
Pablo Triana <pabloandrestrianam@gmail.com>
2023-09-02 18:52:06 -0500

    PB: Add Delete, Put, Get by ID

--- apps/api/src/book/book.controller.ts
+++ apps/api/src/book/book.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { BookService } from './book.service';
 import { Book } from './schemas/book.schemas';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Controller('book')
 export class BookController {
@@ -14,8 +24,33 @@ export class BookController {
   @Post('new')
   async createBook(
     @Body()
-    book,
+    book: CreateBookDto,
   ): Promise<Book> {
     return this.bookService.create(book);
+  }
+
+  @Get(':id')
+  async getBook(
+    @Param('id')
+    id: string,
+  ): Promise<Book> {
+    return this.bookService.findById(id);
+  }
+
+  @Put(':id')
+  async UpdateBook(
+    @Param('id')
+    id: string,
+    @Body()
+    book: UpdateBookDto,
+  ): Promise<Book> {
+    return this.bookService.updateById(id, book);
+  }
+  @Delete(':id')
+  async deleteBook(
+    @Param('id')
+    id: string,
+  ): Promise<Book> {
+    return this.bookService.deleteById(id);
   }
 }
